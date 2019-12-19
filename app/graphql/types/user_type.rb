eef68b7cc93a152640eda4252911986c11c211ef
Denis <ddujota@internationalsafety.com>
2019-12-19 00:16:50 -0500

    cleaned up the demo node

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -3,13 +3,5 @@ module Types
     field :id, ID, null: false
     field :email, String, null: false
     field :profile, Types::ProfileType, null: false
-
-    # TODO: REMOVE THIS TESETER METHOD
-    field :greeting, String, null: false
-
-    def greeting
-      # object references the user instance
-      "Hello your email address is: #{object.email}"
-    end
   end
 end
