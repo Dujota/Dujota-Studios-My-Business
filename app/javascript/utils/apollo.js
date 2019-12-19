6c9072b4beb673fee3fd491e5f921bd503e797e7
Denis <ddujota@internationalsafety.com>
2019-12-19 11:37:13 -0500

    setup create cache

---
+++ app/javascript/utils/apollo.js
@@ -0,0 +1,22 @@
+// In this file we want to configure the two core entities of the Apollo application, the client and the cache (or more precisely, the functions to create both):
+
+// Client
+import { ApolloClient } from 'apollo-client';
+
+// Cache
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+// Links
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink, Observable } from 'apollo-link';
+
+export const createCache = () => {
+  const cache = new InMemoryCache();
+
+  if (process.env.NODE_ENV === 'development') {
+    window.secretVariableToStoreCache = cache;
+  }
+
+  return cache;
+};
