4c08a85454c5b1e8dd5e039e2460c6520cce6fee
Denis <ddujota@internationalsafety.com>
2019-12-21 17:11:19 -0500

    setup link with csrf token to protect from forgery

--- app/javascript/utils/apollo.js
+++ app/javascript/utils/apollo.js
@@ -11,6 +11,7 @@ import { HttpLink } from 'apollo-link-http';
 import { onError } from 'apollo-link-error';
 import { ApolloLink, Observable } from 'apollo-link';
 
+// CREATE CACHE
 export const createCache = () => {
   const cache = new InMemoryCache();
 
@@ -20,3 +21,41 @@ export const createCache = () => {
 
   return cache;
 };
+
+// Get Token from Meta Tags on Application.html.erb
+
+const getToken = () => {
+  document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+};
+const token = getToken();
+
+// MiddleWare Operationt that sets the CSRF token on requests to protect from forgery
+const setTokenForOperation = async operation =>
+  operation.setContext({
+    headers: {
+      'X-CSRF-Token': token,
+    },
+  });
+
+// Link With CSRF Token
+const createLinkWithToken = () =>
+  new ApolloLink(
+    (operation, forward) =>
+      new Observable(observer => {
+        let handle;
+        Promise.resolve(operation)
+          .then(setTokenForOperation)
+          .then(() => {
+            handle = forward(operation).subscribe({
+              next: observer.next.bind(observer),
+              error: observer.error.bind(observer),
+              complete: observer.complete.bind(observer),
+            });
+          })
+          .catch(observer.error.bind(observer));
+
+        return () => {
+          if (handle) handle.unsubscribe();
+        };
+      })
+  );
