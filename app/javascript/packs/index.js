75f540845d142665ff8b186b262409214dc5bc86
Denis <ddujota@internationalsafety.com>
2019-12-22 00:31:24 -0500

    added the render for index.js

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,1 +1,11 @@
-console.log('👻');
+import React from 'react';
+import { render } from 'react-dom';
+import Provider from '../components/Provider';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const mountingPoint = document.getElementById('#root');
+
+  if (mountingPoint) {
+    render(<Provider>👻</Provider>, mountingPoint);
+  }
+});
