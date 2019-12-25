3289330e3e5dfd31e9a455c0f521fe25fda86759
Denis <ddujota@internationalsafety.com>
2019-12-24 23:37:08 -0500

    added current user lookup to the app

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,8 +8,15 @@ module Types
           null: false,
           description: "Returns a list of all Blog Posts in the app"
 
+    field :me, Types::UserType, null: true
+
     def posts
       Post.all
     end
+
+    def me
+      context[:current_user]
+    end
+
   end
 end
