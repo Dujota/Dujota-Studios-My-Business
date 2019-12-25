5e7d7204d2c0eaeeb9f023df3b8be8063fb4b199
Denis <ddujota@internationalsafety.com>
2019-12-25 00:54:32 -0500

    added the sing in mutation

--- app/graphql/mutations/sign_in_mutation.rb
+++ app/graphql/mutations/sign_in_mutation.rb
@@ -8,7 +8,22 @@ module Mutations
     field :user, Types::UserType, null: true
 
     def resolve(email:, password:)
-      # user = User.find_for_database_authentication()
+      # find the user based on the email argument provided to the mutation
+      user = User.find_for_database_authentication(email: email)
+
+      if user.present?
+        # Check that the password for the fetched user, matches the one provided by mutation
+        if user.valid_password?(password: password)
+          context[:current_user] = user
+
+          # TODO:: change this
+          return user
+        else
+          GraphQL::ExecutionError.new("Incorrect Email/Password")
+        end
+      else
+        GraphQL::ExecutionError.new("User with #{email} not registered on this application")
+      end
     end
   end
 end
