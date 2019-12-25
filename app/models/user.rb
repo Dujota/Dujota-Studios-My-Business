257fa11e8bfb01e8ebb7ed3b03d0e3c7d43b44dc
Denis <ddujota@internationalsafety.com>
2019-12-25 00:33:33 -0500

    setup the jwt migration and model as per docs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :jwt_authenticatable, jwt_revocation_strategy: BlackList
+         :jwt_authenticatable, jwt_revocation_strategy: Blacklist
 
   has_many :posts, dependent: :destroy
   has_one :profile, autosave: true
