00a4738133e0c07893a7a26e207edee00722044c
Denis <ddujota@internationalsafety.com>
2019-12-31 13:47:47 -0500

    login works and gettign the current user and its token

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -2,6 +2,7 @@ module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
     field :email, String, null: false
+    field :token, String, null: false
     field :profile, Types::ProfileType, null: false
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
 
   has_many :posts, dependent: :destroy
@@ -12,4 +15,18 @@ class User < ApplicationRecord
            :last_name, :last_name=,
            :full_name, :full_name=,
            to: :profile
+
+  # TODO CHECK
+  def token
+    headers = {
+      "Accept" => "application/json",
+      "Content-Type" => "application/json",
+    }
+    scope ||= Devise::Mapping.find_scope!(self)
+    aud ||= headers[Warden::JWTAuth.config.aud_header]
+    token, payload = Warden::JWTAuth::UserEncoder.new.call(
+      self, scope, aud
+    )
+    token
+  end
 end
